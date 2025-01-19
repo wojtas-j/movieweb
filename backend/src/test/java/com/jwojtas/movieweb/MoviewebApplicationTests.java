0cd076b4e841b8bb86bfd29521331ae4952310fc
kwojtas912@gmail.com <kwojtas912@gmail.com>
2025-01-19 13:57:58 +0100

    Project initial commit

---
+++ backend/src/test/java/com/jwojtas/movieweb/MoviewebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jwojtas.movieweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoviewebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
